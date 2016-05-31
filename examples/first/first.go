7e6e663d872211dec96756f293b6fa31924ffae6
mileusna <mileusna@gmail.com>
2016-05-31 02:04:45 +0200

    Add examples and package documentation

---
+++ examples/first/first.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/mileusna/facebook-messenger"
+)
+
+func main() {
+	msng := &messenger.Messenger{
+		AccessToken:     "YOUR_ACCESS_TOKEN_THAT_YOU_WILL_GENERATE_FOR_YOUR_PAGE_ON_FACEBOOK",
+		PageID:          "YOUR_PAGE_ID",
+		MessageReceived: messageReceived, // your function for handling received messages, defined below
+	}
+
+	// in init or afterwards, you can also specify events when receiving postbacks and message delivery reports from Facebook
+	// if you don't want to manage this events, just comment/don't use this receivers
+	msng.PostbackReceived = postbackReceived // comment/delete if not used
+	// msng.DeliveryReceived = deliveryReceived // comment/delete if not used
+
+	// set URL for your webhook and directly use msng as http Handler
+	http.Handle("/mychatbot", msng)
+	http.ListenAndServe(":8008", nil)
+}
+
+// messageReceived is called when you receive message on you webhook i.e. when someone sends message to your chat bot
+// params: messenger that received the message, then the user id that sent us message and message data itself
+func messageReceived(msng *messenger.Messenger, userID int64, m messenger.FacebookMessage) {
+
+	// message received, now lets check what user has sent to us
+	switch m.Text {
+	case "hello", "hi":
+		// someone sent hello or hi, reply with simple text message
+		msng.SendTextMessage(userID, "Hello there")
+
+	case "send me website":
+		// now lets send him some structured message with image, link and buttons
+		// if we don't need buttons, we can use AddNewElement
+		gm := msng.NewGenericMessage(userID)
+		gm.AddNewElement("Title", "Subtitle", "http://mysite.com", "http://mysite.com/some-photo.jpeg")
+
+		// GenericMessage can contain up to 10 elements, they are represented as cards and can be scoreled horicontally in messenger
+		// So lets add one more element, this time with buttons
+		e := msng.NewElement("Site title", "Subtitle", "http://mysite.com", "http://mysite.com/some-photo.jpeg")
+		e.AddWebURLButton("Contact US", "http://mysite.com/contact")
+		e.AddPostbackButton("Ok", "THIS_DATA_YOU_WILL_RECEIVE_AS_POSTBACK_WHEN_USER_CLICK_THE_BUTTON")
+		gm.AddElement(e)
+
+		// ok, message is ready, lets sent
+		msng.SendMessage(gm)
+
+	default:
+		// upthere we haven't check for errors and responses for cleaner example code
+		// but keep in mind that SendMessage returns FacebookResponse struct and error
+		// errors are received from Facebook if sometnihg went wrong with message sending
+		resp, err := msng.SendTextMessage(userID, m.Text) // echo, send back to user the same text he sent to us
+		if err != nil {
+			log.Println(err)
+			return // if there is an error, resp is empty struct, useless
+		}
+		log.Println("Message ID", resp.MessageID, "sent to user", resp.RecipientID)
+		// store resp.MessageID if you want to track delivery reports that will be sent later from google
+	}
+}
+
+// postbackReceived is called when you reiceive postback event from Facebook server
+func postbackReceived(msng *messenger.Messenger, userID int64, p messenger.FacebookPostback) {
+	if p.Payload == "THIS_DATA_YOU_WILL_RECEIVE_AS_POSTBACK_WHEN_USER_CLICK_THE_BUTTON" {
+		// user just clicked Ok button from previouse example, lets just send him a message
+		msng.SendTextMessage(userID, "Ok, I'm always online, chat with me anytime :)")
+	}
+}
+
+// deliveryReceived is used if you want to track delivery reports for sent messages
+func deliveryReceived(msng *messenger.Messenger, userID int64, d messenger.FacebookDelivery) {
+	for _, mid := range d.Mids {
+		log.Println("Message delivered, msgID:", mid)
+	}
+}
---
+++ examples/second/second.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/mileusna/facebook-messenger"
+)
+
+// use public messenger for simpler code demonstration
+var msng = &messenger.Messenger{
+	AccessToken: "YOUR_ACCESS_TOKEN_THAT_YOU_WILL_GENERATE_FOR_YOUR_PAGE_ON_FACEBOOK",
+	PageID:      "YOUR_PAGE_ID",
+}
+
+// Please check the First example First, it contains more example code for sending messages
+// If you don't want to use events receivers like in First example, this is another approach
+// that will give you entire FacebookRequest struct received from the Facebook so you can
+// use it however you like
+func main() {
+	// set URL for your webhook and you handler func
+	http.HandleFunc("/mychatbot", myHandler)
+	http.ListenAndServe(":8008", nil)
+}
+
+// myHandler is you regular http Handler
+func myHandler(w http.ResponseWriter, r *http.Request) {
+
+	// decode entire request received from Facebook into FacebookRequest struct
+	fbRequest, _ := messenger.DecodeRequest(r)
+
+	// now you have it all and you can do whatever you want with received request
+	// enumerate each entry, and each message in entry
+	for _, entry := range fbRequest.Entry {
+		// pageID := entry.ID  // here you can find page id that received message
+		for _, msg := range entry.Messaging {
+			userID := msg.Sender.ID // user that sent you a message
+
+			// but "message" can be text message, delivery report or postback, so check it what it is
+			// it can only be one of this, so we use switch
+			switch {
+			case msg.Message != nil:
+				log.Println("Msg received with content:", msg.Message.Text)
+				msng.SendTextMessage(userID, "Hello there")
+				// check First example for more sending messages examples
+
+			case msg.Delivery != nil:
+				// delivery report received, check First example what to do next
+
+			case msg.Postback != nil:
+				// postback received, check First example what can you do with that
+				log.Println("Postback received with content:", msg.Postback.Payload)
+			}
+		}
+	}
+}
